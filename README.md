a4cc3abd336e8db4399459f4af9434aededf1941
MAK <maheshkharote2580@gmail.com>
2026-01-02 10:37:37 +0000

    commit: files are updated

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Intership_Project
+
+"""
+water_synthesis.py
+
+Add water effect to an in-air RGB image given a depth map and water parameters.
+This uses Sea-thru's forward formation model:
+    I_c(z) = J_c * exp(-betaD_c(z) * z) + B_inf_c * (1 - exp(-betaB_c * z))
+
+Inputs:
+- J_rgb: HxWx3 float32 image in linear RGB, values in [0,1]
+- z: HxW depth map in meters (absolute)
+- params: dictionary with water parameters (see defaults)
+
+Outputs:
+- I_out: HxWx3 float32 synthesized underwater image in [0,1]
+"""
--- water_synthesis.py
+++ water_synthesis.py
@@ -1,19 +1,3 @@
-"""
-water_synthesis.py
-
-Add water effect to an in-air RGB image given a depth map and water parameters.
-This uses Sea-thru's forward formation model:
-    I_c(z) = J_c * exp(-betaD_c(z) * z) + B_inf_c * (1 - exp(-betaB_c * z))
-
-Inputs:
-- J_rgb: HxWx3 float32 image in linear RGB, values in [0,1]
-- z: HxW depth map in meters (absolute)
-- params: dictionary with water parameters (see defaults)
-
-Outputs:
-- I_out: HxWx3 float32 synthesized underwater image in [0,1]
-"""
-
 import numpy as np
 import cv2
 import imageio
